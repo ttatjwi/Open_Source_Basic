aabd3de036200187ce23865d43a4355d621604ab
ttatjwi <su_zero_@naver.com>
2024-09-13 13:21:10 +0900

    two file change

--- prime_number.py
+++ prime_number.py
@@ -1,6 +1,6 @@
 def is_prime_number(x):
     for num in range(2, x):
-        if x % num == 0:
-            print("The number {num1} is not a prime number.".format(num1=x))
+        if x <= 1:
+            print("Error: The input value should be larger than 1".format(num1=x))
             return
     print("The number {num1} is a prime number.".format(num1=x))
